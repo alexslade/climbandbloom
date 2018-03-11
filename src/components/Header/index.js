219b6f52735a27f0ea7cf99c88dad9d572ae1b5d
Alex Heeton <alex@heeton.me>
2018-03-11 13:41:32 +0000

    Add leaf

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,13 @@ const Header = () => (
   <div className={s.header}>
     <h1>
       Climb and Bloom
+      <svg className={s.leaf} transform="scale(-1,1)" viewBox="0 0 640 480" xmlns="http://www.w3.org/2000/svg">
+        <title>leaf</title>
+        <g>
+          <title>Layer 1</title>
+          <path id="svg_1" d="m595.223816,392.677551c-387.070389,163.258362 -545.501865,-257.993881 -522.358368,-295.871704c23.143494,-37.877827 480.03891,-87.727917 522.358368,295.871704z" fill="#29ca38"/>
+        </g>
+       </svg>
     </h1>
     <h2>
       Radically effective startup support
