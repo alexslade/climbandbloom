6406f7d2cb14b770eded39c060883ebffbce6968
Alex Heeton <alex@heeton.me>
2018-03-11 14:10:26 +0000

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,9 @@ import Helmet from 'react-helmet'
 import { Header } from '../components'
 import s from '../styles/index.module.scss'
 
+import favicon16 from './favicon-16.png';
+import favicon32 from './favicon-32.png';
+
 const TemplateWrapper = ({ children }) => (
   <div className={s.body}>
     <Helmet
@@ -12,8 +15,11 @@ const TemplateWrapper = ({ children }) => (
       meta={[
         { name: 'description', content: 'Radically effecting support for startups and new products' },
         // { name: 'keywords', content: '' },
-      ]}
-    />
+      ]}>
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+    </Helmet>
+
     <div className={s.section}>
       {children()}
     </div>
