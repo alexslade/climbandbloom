5a348d6846825483d4febba3bed61d29390e4632
Alex Heeton <alex@heeton.me>
2018-03-12 18:02:01 +0000

    Update CAB blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ class IndexPage extends React.Component {
       <Box>
         <Header />
         <div className={s.content}>
-          <p> Pommy ipsum posh nosh punter grub's up you mean it ain't me noggin' stiff upper lip, black cab had a barney with the inlaws Sherlock grub's up. Make a brew cobbles it's the bees knees football accordingly by 'eck love. </p>
-          <p> Gutted a bit wonky copper Kate and Will, bossy britches on the beat plum pudding and we all like figgy pudding. Ask your mother if a comely wench copped a bollocking bit of a div they can sod off see a man about a dog big light narky ear hole. </p>
-          <p> Hard cheese old boy doofer bottled it chav I bid you good day bent as a nine bob note darling, wind up had a barney with the inlaws doing my head in it's me peepers.  </p>
-          <p> It's nicked doing my head in copper throw a paddy narky damn, sweets bull dog pie-eyed know your onions. </p>
+          <p> We like to keep things simple. </p>
+          <p> If you have a product or a startup, we specialise in making you more successful. That's it. </p>
+          <p> For brand new founders, we help with technical roadmapping. If you've already launched something, maybe you'd like support on monitoring and anlyising the results. Or if you have a product that's struggling, we can help identify problems with your team and processes. </p>
+          <p> We'll save you time and money, and help your products reach the vision that you've set for them.</p>
         </div>
         <Button to="mailto:alex@climbandbloom.com">Contact us</Button>
       </Box>
